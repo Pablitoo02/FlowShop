c61eb624ccfe523b75b90da85563e7c7424ab7d3
Pablitoo02 <pablo.1hermida@gmail.com>
2023-05-30 01:16:20 +0200

    Home completado
    Creación de los botones "Sweatshirts", "T_shirts" y "Trousers", y sus respectivos fragments.

--- app/app/src/main/java/com/example/flowshop/screens/HomeFragment.java
+++ app/app/src/main/java/com/example/flowshop/screens/HomeFragment.java
@@ -1,60 +1,46 @@
 package com.example.flowshop.screens;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.flowshop.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Context context;
+    private Button sweatshirts, t_shirts, trousers;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    /*private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";*/
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
+    /*public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+        /*if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        }*/
     }
 
     @Override
@@ -62,5 +48,52 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        sweatshirts =  view.findViewById(R.id.sweatshirts);
+        t_shirts =  view.findViewById(R.id.t_shirts);
+        trousers =  view.findViewById(R.id.trousers);
+
+        //Botón "Sweatshirts"
+        sweatshirts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Cambio al fragment "Sweatshirts"
+                SweatshirtsFragment sweatshirts = new SweatshirtsFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameContainer,sweatshirts)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        //Botón "T_shirts"
+        t_shirts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Cambio al fragment "T_shirts"
+                T_shirtsFragment t_shirts = new T_shirtsFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameContainer,t_shirts)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        //Botón "Trousers"
+        trousers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Cambio al fragment "Trousers"
+                TrousersFragment trousers=new TrousersFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameContainer,trousers)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 }
